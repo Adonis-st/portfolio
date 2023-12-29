82f121df302ad803360dac2ce43619facb6ed2ff
Adonis-st <adonisy851@gmail.com>
2023-12-28 23:06:06 -0600

    added new project

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,54 +3,50 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
 const contactFormSchema = z.object({
-	name: z.string(),
-	email: z.string(),
-	message: z.string(),
+  name: z.string(),
+  email: z.string(),
+  message: z.string(),
 });
 type ContactInput = z.infer<typeof contactFormSchema>;
 
 export const ContactForm = () => {
-	const {
-		register,
-		handleSubmit,
-		setValue,
-		watch,
-		control,
-		getValues,
-		formState: { errors },
-	} = useForm<ContactInput>({
-		resolver: zodResolver(contactFormSchema),
-		mode: "onBlur",
-	});
-	const onSubmit = (data: ContactInput) => console.log(data);
-	return (
-		<form
-			onSubmit={handleSubmit(onSubmit)}
-			className="flex flex-col text-white font-medium leading-[1.625rem] tracking-[-.22px] lg:w-[445px]">
-			<input
-				type="text"
-				{...register("name")}
-				placeholder="NAME"
-				className="bg-transparent border-b border-white p-3 focus:outline-none mb-4 "
-			/>
-			<input
-				type="text"
-				{...register("email")}
-				placeholder="EMAIL"
-				className="bg-transparent border-b border-white p-3 focus:outline-none mb-4 "
-			/>
-			<textarea
-				{...register("message")}
-				placeholder="MESSAGE"
-				rows={4}
-				className="bg-transparent border-b border-white p-3 focus:outline-none resize-none mb-8 "
-			/>
+  const { register, handleSubmit } = useForm<ContactInput>({
+    resolver: zodResolver(contactFormSchema),
+    mode: "onBlur",
+  });
 
-			<button
-				type="submit"
-				className="underline decoration-neon underline-offset-[14px] self-end font-bold leading-[1.625rem] tracking-[2.29px]">
-				SEND MESSAGE
-			</button>
-		</form>
-	);
+  const onSubmit = (data: ContactInput) => console.log(data);
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col text-white font-medium leading-[1.625rem] tracking-[-.22px] lg:w-[445px]"
+    >
+      <input
+        type="text"
+        {...register("name")}
+        placeholder="NAME"
+        className="bg-transparent border-b border-white p-3 focus:outline-none mb-4 "
+      />
+      <input
+        type="text"
+        {...register("email")}
+        placeholder="EMAIL"
+        className="bg-transparent border-b border-white p-3 focus:outline-none mb-4 "
+      />
+      <textarea
+        {...register("message")}
+        placeholder="MESSAGE"
+        rows={4}
+        className="bg-transparent border-b border-white p-3 focus:outline-none resize-none mb-8 "
+      />
+
+      <button
+        type="submit"
+        className="underline decoration-neon underline-offset-[14px] self-end font-bold leading-[1.625rem] tracking-[2.29px]"
+      >
+        SEND MESSAGE
+      </button>
+    </form>
+  );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				coal: "#151515",
-				neon: "#4EE1A0",
-				gray: "#d9d9d9",
-				dark_gray: "#242424",
-
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        coal: "#151515",
+        neon: "#4EE1A0",
+        gray: "#d9d9d9",
+        dark_gray: "#242424",
+      },
+    },
+  },
+  plugins: [],
+};
